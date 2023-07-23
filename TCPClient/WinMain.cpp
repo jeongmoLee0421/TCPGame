2e78c47c06ad89e49b8714e867874f4f57cefc27
jeongmo Lee <mermaid3605@naver.com>
2023-07-23 20:48:41 +0900

    tcp client 진입점 코드 생성

---
+++ TCPClient/WinMain.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+// 2023 07 23 이정모 home
+
+// tcp client 진입점
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+
+	return 0;
+}
